b23d4be545bde97e6e7965781b94057b66c0ca0e
NzauM <mercywaenu16@gmail.com>
2025-09-10 10:28:34 +0300

    add appointments class

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # import the Flask class
 from flask import Flask, make_response, request
 from flask_migrate import Migrate
-from models import db, Patient
+from models import db, Patient, Appointment
 # create an instance of this class, mostly called app, this is the instance responsible for runnin/starting our flask application
 app = Flask(__name__)
 # app["SQLALCHEMY_DATABASE_URI"] = "sqlite:///hospital.db"
@@ -10,7 +10,7 @@ db.init_app(app)
 migrate = Migrate(app, db)
 # 
 
-@app.route('/welcome')
+@app.route('/')
 def welcomefunction():
     return '<h3>Welcome to My App</h3>', 200
 
@@ -22,7 +22,7 @@ def login():
 @app.route('/create_patient', methods=['POST'])
 def create_patient():
     # receive patient details from the client request
-    client_data = request.get_json()
+    client_data = request.get_json() 
 
     # and save this patient to the database 
     new_patient = Patient(name=client_data['name'], age=client_data['age'], next_of_kin=client_data['next_of_kin'], patient_no=client_data['patient_no'])
@@ -30,3 +30,37 @@ def create_patient():
     db.session.commit()
     resp = make_response({'message':"Patient created successfully"}, 201)
     return resp
+
+# get all patients
+@app.route('/list_patients')
+def list_patients():
+    # selecting all patients in the patients table and return them.
+    all_patients = db.session.query(Patient).all()
+    # print(all_patients)
+    # querying using sqlalchemy will return instances of the class
+    # we need to format these instances to a more generic format, e.g JSON/Dictionary
+    # hence comes serializers, they enable us to convert an instance to a dictionary, in just one line
+    # {id:1, name:"Patient1"}
+    patient_list = []
+    for patient in all_patients:
+        # patient_dict= {"id":patient.id, "name":patient.name}
+        patient_dict= patient.to_dict()
+        patient_list.append(patient_dict)
+    resp = make_response({'data':patient_list}, 200)
+    return resp
+
+# url path for creating a new appointment
+@app.route('/create_appointment', methods=['POST'])
+def create_appointment():
+    # receiving appointment details from client request
+    appt_details = request.get_json()
+    print(appt_details)
+    # create an instance of the Appointment class and save it to the DB.
+    new_appt = Appointment(patient_id = appt_details['patient_id'], day=appt_details['day'])
+    db.session.add(new_appt)
+    db.session.commit()
+    resp = make_response({'message':"Appointment created successfully"}, 201)
+    return resp
+
+
+
--- models.py
+++ models.py
@@ -1,12 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
-class Patient(db.Model):
+class Patient(db.Model, SerializerMixin):
     '''
     Patient Class created with name, age, next_of_kin, patient_no
     '''
-
+    serialize_rules = ('-appointments.patient',)
     __tablename__ = 'patients'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -14,3 +15,39 @@ class Patient(db.Model):
     age = db.Column(db.Integer)
     next_of_kin = db.Column(db.String)
     patient_no = db.Column(db.String)
+
+    appointments = db.relationship('Appointment',backref='patient')
+    # {appointments:[]}
+
+    pass
+
+class Appointment(db.Model, SerializerMixin):
+    '''
+    Appointment instances to be created with patient_id, notes, doctor_id, day
+    '''
+
+    # serialize_only = ('patient_id','day')
+    __tablename__ = 'appointments'
+
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patients.id')) 
+    # doctor_id = db.Column(db.Integer, db.ForeignKey('doctors.id'))
+    notes = db.Column(db.String, nullable=True)
+    day = db.Column(db.String)
+
+    # {id:1, patient_id:1, notes:'', day:'Tuesday'}
+
+
+
+
+
+# Doctor 1 Doctor treats how many patients? Many. 
+        #1 Patient can be treated by how many doctors? Many. 
+# Appointment
+        # 1 Appointment has how many doctors? One
+        # 1 Doctor can have how many appointments? Many
+
+
+        # 1 Appointment has how many patients? One
+        # 1 Patient can have how many appointments? Many
+# Staff
